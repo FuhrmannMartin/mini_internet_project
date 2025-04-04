46e147f325895a90fc36963268e89c9f61325698
marti <martin.fuhrmann@stud.fh-campuswien.ac.at>
2025-04-04 12:57:26 +0200

    add script to restart web container

--- platform/restart_webserver.sh
+++ platform/restart_webserver.sh
@@ -11,11 +11,6 @@ echo "Stopping and removing old WEB container (if any)..."
 sudo docker stop WEB 2>/dev/null || true
 sudo docker rm WEB 2>/dev/null || true
 
-echo "🚀 Starting new WEB container..."
-sudo docker run --name WEB -d \
-  -v "${HOME}"/mini_internet_project_fork/utils/dummy_traceroutes:/traceroutes:ro \
-  miniinterneteth/d_webserver
-
 echo "Running website setup script..."
 sudo setup/website_setup.sh .
 
